7342010d7a7941df74d66eeedf665a0848979958
Yashi Srivastava <yashi.srivastava@psiborg.in>
2024-05-27 18:28:56 +0530

    chart

--- src/app/admin/account-management/page.tsx
+++ src/app/admin/account-management/page.tsx
@@ -1,7 +1,45 @@
+"use client";
 import React from "react";
+import CustomGraph from "@/components/charts/index";
+interface ViewCountData {
+  _id: string;
+  views: number;
+}
 
 const AccountManagementPage = () => {
-  return <div>Account Management</div>;
+  const viewCount: ViewCountData[] = [
+    {
+      _id: "20/03/2023",
+      views: 45,
+    },
+    {
+      _id: "24/03/2023",
+      views: 23,
+    },
+    {
+      _id: "25/03/2023",
+      views: 5,
+    },
+    {
+      _id: "27/03/2023",
+      views: 16,
+    },
+    {
+      _id: "29/03/2023",
+      views: 35,
+    },
+    {
+      _id: "30/03/2023",
+      views: 30,
+    },
+  ];
+
+  return (
+    <div>
+      Account Management
+      <CustomGraph viewCount={viewCount} />
+    </div>
+  );
 };
 
 export default AccountManagementPage;
---
+++ src/components/charts/index.tsx
@@ -0,0 +1,94 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+interface ViewCountData {
+  _id: string;
+  views: number;
+}
+interface CustomBarProps {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: string;
+}
+const CustomRoundedBar: React.FC<CustomBarProps> = ({
+  x,
+  y,
+  width,
+  height,
+  fill,
+}) => {
+  // Define the arc radius for rounding the top corners
+  const radius = 10;
+  return (
+    <path
+      d={`
+        M${x},${y + height}
+        v${-height + radius}
+        a${radius},${radius} 0 0,1 ${radius},-${radius}
+        h${width - 2 * radius}
+        a${radius},${radius} 0 0,1 ${radius},${radius}
+        v${height - radius}
+        z
+      `}
+      fill={fill}
+    />
+  );
+};
+// Define the props for CustomGraph
+interface CustomGraphProps {
+  viewCount: ViewCountData[];
+}
+const CustomGraph: React.FC<CustomGraphProps> = ({ viewCount }) => {
+  const colors = {
+    steps: {
+      stroke: "#28844B",
+      fill: "#28844B",
+    },
+    text: "#697077",
+    background: "#fff",
+  };
+  return (
+    <ResponsiveContainer height={600} width="100%">
+      <BarChart data={viewCount}>
+        <XAxis
+          dataKey="_id"
+          tickSize={8}
+          tickMargin={15}
+          tick={{ fill: colors.text }}
+          tickLine={{ stroke: colors.text }}
+          height={50}
+        />
+        <YAxis
+          tick={{ fill: colors.text }}
+          tickLine={{ stroke: colors.text }}
+        />
+        <CartesianGrid strokeDasharray="1" />
+        <Tooltip
+          contentStyle={{
+            backgroundColor: colors.background,
+            color: "#222",
+          }}
+          itemStyle={{ color: colors.steps.stroke }}
+        />
+        <Bar
+          dataKey="views"
+          stroke={colors.steps.stroke}
+          fill={colors.steps.fill}
+          strokeWidth={6}
+          barSize={18}
+          //   shape={CustomRoundedBar}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+export default CustomGraph;
